025d1afb64a0350171ea189927332c718c4ce1a9
Richmond Nunoo <1957richmond@gmail.com>
2023-08-14 14:46:20 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/pharmacy_pos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pharmacy_pos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:pharmacy_pos/views/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Restaurant UI',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
